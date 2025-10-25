ac0d0a55b8b8c0d0815ba12b5cf4a499d7ea3af0
Bhuvaneshwar-bit <bhuvaneshwar20866@gmail.com>
2025-10-25 21:15:05 +0530

    Fix: Remove interval prop from BackgroundTransition component usage

--- app/edii-tn/page.tsx
+++ app/edii-tn/page.tsx
@@ -99,10 +99,9 @@ export default function EDIITNPage() {
       
       {/* Hero Section */}
       <section ref={heroRef} className="relative min-h-screen flex items-center justify-center overflow-hidden pt-20">
-        {/* Smooth Background Image Transition */}
+        {/* Static Background Image */}
         <BackgroundTransition 
           images={['/images/one.JPG', '/images/two.JPG']} 
-          interval={6000}
         />
 
         <div className="relative z-10 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-32">
