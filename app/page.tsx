8106a2cd243fbb3fe1cfe307a0ae2369e5b1e8a4
Bhuvaneshwar-bit <bhuvaneshwar20866@gmail.com>
2025-11-21 17:23:41 +0530

    Remove i-ACE Tribe section from homepage

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,6 @@ import CorePillars from '@/components/sections/CorePillars';
 import MuAISpotlight from '@/components/sections/MuAISpotlight';
 import Partners from '@/components/sections/Partners';
 import CampusExperience from '@/components/sections/CampusExperience';
-import IACETribe from '@/components/sections/IACETribe';
 import Footer from '@/components/sections/Footer';
 
 export default function Home() {
@@ -16,7 +15,6 @@ export default function Home() {
       <MuAISpotlight />
       <Partners />
       <CampusExperience />
-      <IACETribe />
       <Footer />
     </main>
   );
